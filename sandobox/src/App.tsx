3dbe0aad08f83956d8d721cf475297052d611963
DynamicDaikon <hietasoba@gmail.com>
2024-12-15 16:55:50 +0900

    [fix] confirm communication to API

--- sandobox/src/App.tsx
+++ sandobox/src/App.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const getApi = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/sample');
+      console.log('response');
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log('send');
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <div>================ ↓ area for free ↓ ================</div>
+      <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+        hello
+      </Box>
+      <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+        <button onClick={getApi}>API通信</button>
+      </Box>
     </>
   );
 }
