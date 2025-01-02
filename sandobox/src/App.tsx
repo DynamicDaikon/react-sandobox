afe53e507bd94f57c5cdefef209eea274e25db6e
DynamicDaikon <hietasoba@gmail.com>
2025-01-02 16:51:06 +0900

    [fix] add useEffect

--- sandobox/src/App.tsx
+++ sandobox/src/App.tsx
@@ -1,25 +1,25 @@
-import Box from '@mui/material/Box';
-import axios from 'axios';
+import Box from "@mui/material/Box";
+import axios from "axios";
 
 function App() {
   const getApi = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/sample');
-      console.log('response');
+      const response = await axios.get("http://localhost:8080/sample");
+      console.log("response");
       console.log(response);
     } catch (error) {
       console.log(error);
     } finally {
-      console.log('send');
+      console.log("send");
     }
   };
 
   return (
     <>
-      <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
         hello
       </Box>
-      <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
         <button onClick={getApi}>API通信</button>
       </Box>
     </>
